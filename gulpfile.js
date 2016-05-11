774b301324f9e3e0bc64cde791848b588168cb76
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-05-11 12:09:51 -0700

    adding files for lint and test

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 const gulp = require('gulp');
+const express = require('express');
+const http = require('http');
 const webpack = require('webpack-stream');
 const eslint = require('gulp-eslint');
+const gp = require('gulp-protractor');
+const webdriver_standalone = require('gulp-protractor').webdriver_standalone;
+
+const server = http.createServer(express().use(express.static(__dirname + '/build/')));
+
+const app = express();
+app.listen(5000, () => console.log('up on 5000 from gulp'));
 
 var files5 = [
   'gulpfile.js',
@@ -10,6 +19,25 @@ var files6 = ['app/js/*.js',
   'gulpfile.js',
   'server.js'
 ];
+
+gulp.task('webdriver_standalone', webdriver_standalone);
+
+gulp.task('e2e_test', function(done) {
+  gulp.src(['test/integration/**/*.js'], { read: false })
+  .pipe(gp.protractor({
+    configFile: './test/integration/config.js',
+    args: ['--baseUrl', 'http://localhost:5000/build/index.html']
+  })).on('error', function(err) {
+    server.close();
+    console.log(err);
+    done();
+  })
+  .on('end', function() {
+    server.close();
+    done();
+  });
+});
+
 
 gulp.task('lint:6', () => {
   return gulp.src(files6)
@@ -51,5 +79,7 @@ gulp.task('static:dev', () => {
 .pipe(gulp.dest('./build'));
 });
 
-gulp.task('build:dev', ['lint:6', 'lint:5', 'webpack:dev', 'static:dev']);
+gulp.task('build:dev', ['webpack:dev', 'static:dev', 'lint:6', 'lint:5', 'webdriver_standalone', 'e2e_test' ]);
+// gulp.task('e2e_test');
+
 gulp.task('default', ['build:dev']);
