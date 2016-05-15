c0587cf629e07d80757e69afc0f16a69162463b3
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-05-15 00:16:49 -0700

    adding second controller

--- client/app/js/entry.js
+++ client/app/js/entry.js
@@ -43,3 +43,40 @@ mvApp.controller('MugsController', ['$http', function($http) {
   };
 
 }]);
+
+
+
+
+mvApp.controller('VinylController', ['$http', function($http) {
+  this.vinyl = [];
+  this.getAll = () => {
+    $http.get(baseUrl + '/api/vinyl')
+    .then((res) => {
+      this.vinyl = res.data;
+    }, handleError.bind(this));
+  };
+
+
+  this.createVinyl = () => {
+    $http.post(baseUrl + '/api/vinyl', this.newVinyl)
+    .then((res) => {
+      this.vinyl.push(res.data);
+      this.newVinyl = null;
+    }, handleError.bind(this));
+  };
+
+  this.updateVinyl = (vinyl) => {
+    $http.put(baseUrl + '/api/vinyl/' + vinyl._id, vinyl)
+    .then(() => {
+      vinyl.editing = false;
+    }, handleError.bind(this));
+  };
+
+  this.removeVinyl = (vinyl) => {
+    $http.delete(baseUrl + '/api/vinyl/' + vinyl._id)
+    .then(() => {
+      this.vinyl.splice(this.vinyl.indexOf(vinyl), 1);
+    }, handleError.bind(this));
+  };
+
+}]);
