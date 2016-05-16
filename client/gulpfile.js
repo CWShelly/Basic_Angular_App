0f137be0e3a70f60b4ce6fca1b2407d079911d4c
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-05-16 11:00:12 -0700

    adding linting

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,9 +1,24 @@
 const gulp = require('gulp');
 const protractor = require('gulp-protractor').protractor;
 const cp = require('child_process');
+const eslint = require('gulp-eslint');
 const webpack = require('webpack-stream');
 const mongoUri = 'mongodb://localhost/test_mvserver';
 var children = [];
+
+var serverFiles = ['../server.js', '../lib/**/*.js', '../test/**/*.js'];
+var clientFiles = ['app/**/*.js', 'server.js', 'gulpfile.js', 'test/**/*.js'];
+
+gulp.task('lint:server', () => {
+  return gulp.src(serverFiles)
+  .pipe(eslint())
+  .pipe(eslint.format());
+});
+gulp.task('lint:client', () => {
+  return gulp.src(clientFiles)
+  .pipe(eslint())
+  .pipe(eslint.format());
+});
 
 gulp.task('webpack:dev', () => {
   gulp.src('app/js/entry.js')
@@ -64,7 +79,7 @@ gulp.task('static:dev', () => {
 
 });
 
-
+gulp.task('lint:files', ['lint:client', 'lint:server']);
 gulp.task('protractor:tests', ['startservers:test', 'protractor:mugtest', 'protractor:vinyltest']);
 
 gulp.task('build:dev', ['webpack:dev', 'static:dev']);
