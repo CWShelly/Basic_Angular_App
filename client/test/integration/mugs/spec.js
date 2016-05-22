c96bfc25ffbaa4fda2a42924bf2e9555479762ff
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-05-21 17:13:36 -0700

    updating e2e tests

--- client/test/integration/mugs/spec.js
+++ client/test/integration/mugs/spec.js
@@ -1,12 +1,23 @@
 describe('all the mugs', function() {
   it('should create a mug', function() {
     browser.get('http://localhost:5000');
-    element(by.model('mugsctrl.newMug.place')).sendKeys('test place');
-    element(by.model('mugsctrl.newMug.city')).sendKeys('test city');
-    element(by.model('mugsctrl.newMug.drinkPref')).sendKeys('test drink');
-    element(by.buttonText('Create Mug')).click();
+    element(by.model('mug.place')).sendKeys('test place');
+    element(by.model('mug.city')).sendKeys('test city');
+    element(by.model('mug.drinkPref')).sendKeys('test drink');
+    element(by.buttonText('Create Mug!')).click();
     element.all(by.repeater('MugsController as mugsctrl')).getText(function(text) {
       expect(text).toEqual('Got this mug at test place in test city. I fill it with test drink');
     });
   });
+
+  it('should create a vinyl', function() {
+    element(by.model('vinyl.album')).sendKeys('test album');
+    element(by.model('vinyl.artist')).sendKeys('test artist');
+    element(by.model('vinyl.purchasedAt')).sendKeys('test store');
+    element(by.buttonText('Create Vinyl!')).click();
+    element.all(by.repeater('VinylController as vinylctrl')).getText(function(text) {
+      expect(text).toEqual('test album by test artist was purchased at test store');
+    });
+  });
+
 });
