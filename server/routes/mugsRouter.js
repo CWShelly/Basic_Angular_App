466f394196794ca6d3243631a6eaa0a2e2d814dd
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air-2.local>
2016-06-01 17:24:29 -0700

    putting server-side auth back. Doesn't currently work

--- server/routes/mugsRouter.js
+++ server/routes/mugsRouter.js
@@ -2,10 +2,11 @@ const Router = require('express').Router;
 const Mug = require(__dirname + '/../models/mug');
 const jsonParser = require('body-parser').json();
 const eH = require(__dirname + '/../lib/error_handler.js');
+const jwtAuth = require(__dirname + '/../lib/jwt_auth');
 var mugRouter = module.exports = Router();
 
 
-mugRouter.post('/mugs', jsonParser, (req, res) => {
+mugRouter.post('/mugs', jwtAuth, jsonParser, (req, res) => {
   var newMug = new Mug(req.body);
   newMug.save((err, data) => {
     if (err) return eH(err, res);
@@ -21,7 +22,7 @@ mugRouter.get('/mugs', (req, res) => {
 });
 
 
-mugRouter.put('/mugs/:id', jsonParser, (req, res) => {
+mugRouter.put('/mugs/:id', jwtAuth, jsonParser, (req, res) => {
   var mugData = req.body;
   delete mugData._id;
   Mug.update({ _id: req.params.id }, mugData, (err) => {
